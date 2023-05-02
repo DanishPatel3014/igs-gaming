f6dfeac4ee32ef300f51ab9a5f104fca8738b29f
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-05-02 07:07:20 -0700

    updates

--- app/page.js
+++ app/page.js
@@ -1,16 +1,24 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
- <section class="s s1" data-scroll-section>
-            <h1 data-scroll>Danish Patel</h1>
-            <p data-scroll>Hello Im Front End Developer👋</p>
-        </section>
-        <section class="s s2" data-scroll-section>
-            <h1 data-scroll data-scroll-speed="1.2">Danish Patel</h1>
-            <p data-scroll data-scroll-speed="2">Hello Im Front End Developer👋</p>
-        </section>
+      <section class="s s1" data-scroll-section>
+        <h1 data-scroll data-scroll-speed="1.2">
+          Danish Patel
+        </h1>
+        <p data-scroll data-scroll-speed="1.5">
+          Hello Im Front End Developer👋
+        </p>
+      </section>
+      <section class="s s2" data-scroll-section>
+        <h1 data-scroll data-scroll-speed="1.2" data-scroll-delay="0.01">
+          Danish Patel
+        </h1>
+        <p data-scroll data-scroll-speed="1.5" data-scroll-delay="0.01">
+          Hello Im Front End Developer👋
+        </p>
+      </section>
     </main>
-  )
+  );
 }
