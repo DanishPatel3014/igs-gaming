3129d393402fff62b3f34e96c060248d2b423d2f
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-05-08 07:30:18 -0700

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,7 @@ setTimeout(()=>{
 }, [])
 
 
+
   const scrollRef = useRef(null);
   useEffect(() => {
     import("locomotive-scroll").then(locomotiveModule => {
@@ -36,6 +37,7 @@ setTimeout(()=>{
       })
     })
   }, [])
+
 
 
   return (
