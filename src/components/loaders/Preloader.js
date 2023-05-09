a0dc1320b1dfd612e65ec9d982c62b83762a922a
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-05-09 07:58:38 -0700

    updates

--- src/components/loaders/Preloader.js
+++ src/components/loaders/Preloader.js
@@ -1,29 +1,18 @@
 "use client";
 
 import { useRef, useEffect , React, useState} from 'react';
-import { gsap } from "gsap";
+
 
 
 function Preloader() {
 
-        const myText = new SplitType('#my-text')
 
-        gsap.to('.char', {
-            y: 0,
-            stagger: 0.05,
-            delay: 0.2,
-            duration: .1
-        })
-
-text.start();
-text.reveal(4000);
 
   return (
     <>
       <div className="preloader" >
-        <div className="pre-ctn">        
-          <span id="my-text" class="glitch" data-text="innovative Game Studio">Innovative Game Studio</span>
-          
+        <div  className="pre-ctn">        
+          <span className="glitch" data-text="innovative Game Studio">Innovative Game Studio</span>
         </div>
       </div>
     </>
