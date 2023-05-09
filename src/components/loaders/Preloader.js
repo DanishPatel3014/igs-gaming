0ce756ddb0467eee7ba9207d8575a773c63a1c0c
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-05-09 07:33:15 -0700

    updates

--- src/components/loaders/Preloader.js
+++ src/components/loaders/Preloader.js
@@ -6,15 +6,24 @@ import { gsap } from "gsap";
 
 function Preloader() {
 
-  
+        const myText = new SplitType('#my-text')
+
+        gsap.to('.char', {
+            y: 0,
+            stagger: 0.05,
+            delay: 0.2,
+            duration: .1
+        })
+
+text.start();
+text.reveal(4000);
 
   return (
     <>
       <div className="preloader" >
         <div className="pre-ctn">        
-          <span>Innovative</span>
-          <span>Game</span>
-          <span>Studio</span>
+          <span id="my-text" class="glitch" data-text="innovative Game Studio">Innovative Game Studio</span>
+          
         </div>
       </div>
     </>
