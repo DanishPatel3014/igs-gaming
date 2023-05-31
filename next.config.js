bc99aaa3645ae6f3adcb94c402215c24e6af3668
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-05-31 16:55:25 +0500

    updates

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['three'],
   experimental: {
     appDir: true,
   },
 }
 
 module.exports = nextConfig
+
+
--- src/components/models/Bannner.js
+++ src/components/models/Bannner.js
@@ -1,100 +1,142 @@
 "use client";
-import React, { useEffect } from 'react'
-import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import GUI from 'lil-gui';
- 
+import React, { useEffect } from "react";
+import { useLoader } from "react-three-fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import * as dat from "lil-gui";
 
-
-   
 export default function Bannner() {
+  useEffect(() => {
+    // =====START=====//
 
-useEffect(() => {
+    // Debug
+    const gui = new dat.GUI();
 
-// =====START=====//
+    // Add Canvas
 
- // Add Canvas
+    const canvas = document.querySelector("canvas.webgl");
 
- const canvas = document.querySelector('canvas.webgl')
+    // Add Scene
+    const scene = new THREE.Scene();
 
- // Add Sizes
- const sizes = {
-   width : innerWidth,
-   height : innerHeight
- }
+    // Add Sizes
+    const sizes = {
+      width: innerWidth,
+      height: innerHeight,
+    };
 
-//  Add Resize Funcation
-window,addEventListener('resize', ()=>{
+    // model
+// const dracoloader = new DRACOLoader();
+// dracoloader.setDecoderPath("/draco/");
 
-// Update sizes
-(sizes.width = window.innerWidth),(sizes.height = window.innerHeight)
+const gltfLoader = new GLTFLoader();
+// gltfLoader.setDRACOLoader(dracoloader);
 
+let mixer = null;
+let model;
 
-// Update Camera
-camera.aspect = sizes.width / sizes.height
-camera.updateProjectionMatrix()
+  const gltf = useLoader(GLTFLoader,'../src/assets/models/Man/glTF/Man.gltf');
+  model = gltf.scene;
 
-// Update Renderer
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2));
+    model.rotation.y = 0.27;
+    console.log(model);
+    scene.add(model);
 
-})
- 
- // Add Scene
- const scene = new THREE.Scene()
- 
- // Add Object
- 
- const mesh = new THREE.Mesh(
-   new THREE.BoxGeometry(1,1,1,5,5,5),
-   new THREE.MeshBasicMaterial({color : 0xff0000})
- )
- scene.add(mesh)
- 
- 
- //Add Camera
- const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height,0.1,100)
- camera.position.z = 3
- camera.lookAt(mesh.position)
- scene.add(camera)
- 
- 
- // Add Renderer
- const renderer = new THREE.WebGLRenderer({
-   canvas: canvas
- })
- renderer.setSize(sizes.width, sizes.height)
- renderer.render(scene, camera)
+    const target = new THREE.Object3D();
+    scene.add(target);
+    mixer = new THREE.AnimationMixer(model);
+    const action = mixer.clipAction(gltf.animations[0]);
 
-// Animation Time past clear
-const clock = new THREE.Clock()
-let pastTime = 0
+    action.play();
+// Material
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.4;
 
-const tick = () =>
-{
-  const elapsedTime = clock.getElapsedTime()
-  const deltaTime = elapsedTime - pastTime
-  pastTime = elapsedTime
+    //  Add Resize Funcation
+    window,
+      addEventListener("resize", () => {
+        // Update sizes
+        (sizes.width = window.innerWidth), (sizes.height = window.innerHeight);
 
-// Update Render
-renderer.render(scene, camera)
+        // Update Camera
+        camera.aspect = sizes.width / sizes.height;
+        camera.updateProjectionMatrix();
 
-  // Call tick again on the next frame
-window.requestAnimationFrame(tick);
-}
+        // Update Renderer
+        renderer.setSize(sizes.width, sizes.height);
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+      });
 
-tick()
- // =====END=====//
+    // Add Object
 
-}, [])
+    const mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    );
+    scene.add(mesh);
 
+    //Add Camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      0.1,
+      100
+    );
+    camera.position.y = 2.12;
+    camera.position.z = 1.6;
+    scene.add(camera);
+
+    // Add Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+      alpha: true,
+    });
+
+    // renderer.shadowMap.enabled = true;
+    // renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.toneMapping = THREE.LinearToneMapping;
+    renderer.toneMappingExposure = 1;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    // Animation Time past clear
+    const clock = new THREE.Clock();
+    let pastTime = 0;
+
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+      const deltaTime = elapsedTime - pastTime;
+      pastTime = elapsedTime;
+
+      // update mixer
+      if (mixer) {
+        mixer.update(deltaTime);
+      }
+
+      // Update camera
+      if (model) {
+        model.rotation.y = cursor.x * 0.8;
+      }
+      // Update controls
+      //  controls.update()
+
+      // Render
+      renderer.render(scene, camera);
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick);
+    };
+
+    tick();
+    // =====END=====//
+  }, []);
 
   return (
     <>
-    <section data-scroll-section  className='mainbanner'>
-      <canvas className='webgl'></canvas>
-    </section>
+      <section data-scroll-section className="mainbanner">
+        <canvas className="webgl"></canvas>
+      </section>
     </>
-  )
+  );
 }
