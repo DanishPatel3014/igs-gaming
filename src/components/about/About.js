545fa851347a2e056f37b14d71000a5787a0e19e
Danish Patel <93546025+DanishPatel3014@users.noreply.github.com>
2023-07-06 05:29:36 -0700

    updates

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,17 +1,30 @@
+import Link from "next/link";
 import React from "react";
+import { BsArrowUpRight } from 'react-icons/bs';
 
 export default function About() {
   return (
     <>
-      <section className="about-sec py-28" data-scroll-section>
-        <div className="container  mx-auto" >
-          <div className="h-wrp" data-scroll data-scroll-delay="0.06" data-scroll-speed="5">
+      <section className="about-sec py-36" data-scroll-section>
+        <div className="container  mx-auto"  >
+        <div className="h-wrp mb-28">
             <h1>
             We Bring Whole-assed gaming<br/>Creative Solutions
             </h1>
             <h2><span>*</span> never Half <span>*</span></h2>
             <h1>Into The World That Solve, Achieve, & Inspire.</h1>
-          </div>
+            </div>
+            <div className="grid grid-cols-2  items-center">
+                <div className="btn-wrap ">
+                    <ul className="btn-list">
+                        <li><Link href={''}>what we do<BsArrowUpRight/></Link></li>
+                        <li><Link href={''}>meet us<BsArrowUpRight/></Link></li>
+                    </ul>
+                </div>
+                <div className="p1 para">
+                If you ask our clients what it’s like working with 36, they’ll talk about how much we care about their success. For us, real relationships fuel real success. We love building brands and helping clients conquer the world with them.
+                </div>
+            </div>
         </div>
       </section>
     </>
