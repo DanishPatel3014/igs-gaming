a56945fd88148bd542190bed54db148346b73af8
safeer-shaikh <93552118+safeerinnovative@users.noreply.github.com>
2023-08-16 14:28:26 +0500

    Update Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,9 +2,36 @@ import React from 'react'
 import { BsArrowUpRight } from 'react-icons/bs';
 
 export default function Contact() {
+
+  const [formData, setFormData] = useState({
+    action: 'CONTACT',
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
+
+  const submitContactForm = async () => {
+    let response = await POST('/mailtest/emailer.php', formData,
+      {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    );
+
+    setFormData({
+      action: 'CONTACT',
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+    });
+
+    alert(response?.message);
+  }
+
   return (
     <>
-    <section className="team-sec py-48" data-scroll-section>
+      <section className="team-sec py-48" data-scroll-section>
         <div className="container mx-auto">
           <div className="sr-wrap mb-36">
             <ul className="slist mb-20">
@@ -16,41 +43,41 @@ export default function Contact() {
               <li>
                 <div className="m-h col-span-2">
                   <h1>
-                  Hey! Lets chat
+                    Hey! Lets chat
                     <br />
                     about your project
                   </h1>
                 </div>
               </li>
             </ul>
-           
+
           </div>
           <div className='ct-wrap'>
             <form>
-                <div className='ct-bx'>
-                    <label>Whats your name?</label>
-                    <input placeholder='Full Name' type='text' />
-                </div>
-                <div className='ct-bx'>
-                    <label>Your email?</label>
-                    <input placeholder='Email Address' type='email' />
-                </div>
-                <div className='ct-bx'>
-                    <label>Whats your number?</label>
-                    <input placeholder='Phone Number' type='number' />
-                </div>
-                <div className='ct-bx'>
-                    <label>Tell us about your project</label>
-                    <input placeholder='Project overview' type='text' />
-                </div>
-                <div className='ct-btn'>
-                   
-                   <button>Submit Form<span className='arw'><BsArrowUpRight /></span></button>
-                </div>
+              <div className='ct-bx'>
+                <label>Whats your name?</label>
+                <input placeholder='Full Name' type='text' onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+              </div>
+              <div className='ct-bx'>
+                <label>Your email?</label>
+                <input placeholder='Email Address' type='email' onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+              </div>
+              <div className='ct-bx'>
+                <label>Whats your number?</label>
+                <input placeholder='Phone Number' type='number' onChange={(e) => setFormData({ ...formData, phone: e.target.value })} />
+              </div>
+              <div className='ct-bx'>
+                <label>Tell us about your project</label>
+                <input placeholder='Project overview' type='text' onChange={(e) => setFormData({ ...formData, message: e.target.value })} />
+              </div>
+              <div className='ct-btn'>
+
+                <button onClick={() => submitContactForm()}>Submit Form<span className='arw'><BsArrowUpRight /></span></button>
+              </div>
             </form>
           </div>
-          </div>
-          </section>
+        </div>
+      </section>
     </>
   )
 }
